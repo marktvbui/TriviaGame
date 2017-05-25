971368b82c71edae3dd7f45467e2ea79e044a6ed
Mark Bui <tvyybui@yahoo.com>
2017-05-25 02:58:02 -0700

    dried up index, added countdown function in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-var countDown = 100;
+var time = 25;
 var correct = 0;
 var wrong = 0;
 var na = 0;
+var intervalId;
+var answer = 'moe';
 var quiz = {
   question: 'Who is not one of the three stooges?',
   answers: [{
@@ -15,18 +17,44 @@ var quiz = {
     answer: 'Larry'
     }]
 };
-function timer(){
-  $('#timer').html('<h2>Remaining Time: ' + countDown +'</h2>');
-};
 
+intervalId = setInterval(count, 1000);
+function count() {
+  time--;
+  console.log(time);
+  if (time === '0') {
+    time;
+  }
+}
+function timer(){
+  $('#timer').html('<h2>Remaining Time: ' + time +'</h2>');
+};
 function question() {
   $('#questions').append('<h3>' + quiz.question + '</h3>');
   for (i = 0; i < quiz.answers.length; i++) {
     $('#questions').append('<h4>' + quiz.answers[i].answer + '</h4>').prop('checked', true);
   }
 };
+function results() {
+  if (answer === 'Joe') {
+    wins++;
+  }
+  else {
+    wrong ++;
+  }
+}
+
+$('#button').click(function() {
+  $('#results').css('visibility', 'visible');
+  $('#container').css('visibility', 'hidden');
+  $('#correct').html('Correctly answered: ' + correct);
+  $('#wrong').html('Answered Wrong: ' + wrong);
+  $('#na').html('Questions with no answer: ' + na);
+});
 
 timer();
 question();
+results();
+count();
 
 });
